5b61e9b3919cff108fee1777dbbad37a940bbb80
davidgape89 <davidgape89@gmail.com>
2018-08-21 20:17:17 +0100

    test(number-system): Included error handling.

--- projects/number-system/src/lib/number-system.spec.ts
+++ projects/number-system/src/lib/number-system.spec.ts
@@ -2,28 +2,47 @@ import NumberSystem from './number-system';
 
 describe('number system class', () => {
 
-  it('should convert 5 in base 10 to 101 in base 2', () => {
-    const result = NumberSystem.convertFromDecimal('5', 2);
+  describe('convertFromDecimal static method', () => {
 
-    expect(result).toEqual('101');
+    it('should convert 5 in base 10 to 101 in base 2', () => {
+      const result = NumberSystem.convertFromDecimal('5', 2);
+
+      expect(result).toEqual('101');
+    });
+
+    it('should convert 120 in base 10 to 231 in base 7', () => {
+      const result = NumberSystem.convertFromDecimal('120', 7);
+
+      expect(result).toEqual('231');
+    });
+
+    it('should throw an error when the base is out of range', () => {
+      expect(() => {
+        NumberSystem.convertFromDecimal('120', 120);
+      }).toThrow();
+    });
+
   });
 
-  it('should convert 120 in base 10 to 231 in base 7', () => {
-    const result = NumberSystem.convertFromDecimal('120', 7);
+  describe('convertToDecimal static method', () => {
+    it('should convert 101 in base 2 to 5 in base 10', () => {
+      const result = NumberSystem.convertToDecimal('101', 2);
 
-    expect(result).toEqual('231');
-  });
+      expect(result).toEqual('5');
+    });
 
-  it('should convert 101 in base 2 to 5 in base 10', () => {
-    const result = NumberSystem.convertToDecimal('101', 2);
+    it('should convert 231 in base 7 to 120 in base 10', () => {
+      const result = NumberSystem.convertToDecimal('231', 7);
 
-    expect(result).toEqual('5');
-  });
+      expect(result).toEqual('120');
+    });
 
-  it('should convert 231 in base 7 to 120 in base 10', () => {
-    const result = NumberSystem.convertToDecimal('231', 7);
+    it('should throw an error when the base is out of range', () => {
+      expect(() => {
+        NumberSystem.convertToDecimal('120', 120);
+      }).toThrow();
+    });
 
-    expect(result).toEqual('120');
   });
 
 });
