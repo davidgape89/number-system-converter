fe7aa7e4d66e0021664fe67bbd62f2ebee575368
David Garcia Perez <davidgarcia@Davids-MacBook-Pro-2.local>
2018-08-23 13:43:17 +0100

    feat(number-system): Used component in sample app.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NumberSystemModule } from 'number-system';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NumberSystemModule
   ],
   providers: [],
   bootstrap: [AppComponent]
