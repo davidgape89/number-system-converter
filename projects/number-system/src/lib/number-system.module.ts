7be1db9b1fc505703f96422bd25fc5837e9e42a0
David Garcia Perez <davidgarcia@Davids-MacBook-Pro-2.local>
2018-08-23 13:42:43 +0100

    feat(number-system): Included angular elements.

--- projects/number-system/src/lib/number-system.module.ts
+++ projects/number-system/src/lib/number-system.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { createCustomElement } from '@angular/elements';
 import { NumberSystemComponent } from './number-system.component';
 
 @NgModule({
   imports: [
+    CommonModule,
+    FormsModule
   ],
   declarations: [NumberSystemComponent],
   exports: [NumberSystemComponent]
 })
-export class NumberSystemModule { }
+export class NumberSystemModule { 
+  constructor(private injector: Injector) {
+    const numberSystemConverter = createCustomElement(NumberSystemComponent, {injector});
+    customElements.define('number-system-converter', NumberSystemComponent);
+  }
+}
